e017063877fe366b7d80fb1545267e831d9b7002
Heon Soo Park <hspark220@gmail.com>
2023-03-13 13:40:38 -0400

    Start on knightsTravail

--- knightsTravail.js
+++ knightsTravail.js
@@ -9,55 +9,100 @@ class Board {
         if (x > width || x < 0 || y < 0 || y > height) return false;
         return true;
     }
+
+    mark(x,y) {
+        board[x][y] = 'x';
+    }
+
+    isFull() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                if (this.board[i][j] != 'x') return false;
+            }
+        }
+        return true;
+    }
+
+    clear() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                this.board[i][j] = '';
+            }
+        }
+    }
 }
 
 class Knight {
-    constructor (startX = 0, startY= 0) {
+    constructor (startX = 0, startY= 0, 
+        leftUp = null, upLeft = null, rightUp = null, upRight = null,
+        leftDown = null, downLeft = null, rightDown = null, downRight = null) {
         this.position = [startX, startY]
+        this.leftUp = leftUp;
+        this.upLeft = upLeft;
+        this.rightUp = rightUp;
+        this.upRight = upRight;
+        this.leftDown = leftDown;
+        this.downLeft = downLeft;
+        this.rightDown = rightDown;
+        this.downRight = downRight;
     }
 
-    //different possible movements
-    moveLeftUp () {
-        this.position[0] -= 1;
-        this.position[1] += 2;
-    }
+    // //different possible movements
+    // moveLeftUp () {
+    //     this.position[0] -= 1;
+    //     this.position[1] += 2;
+    // }
 
-    moveUpLeft () {
-        this.position[0] -= 2;
-        this.position[1] += 1;
-    }
+    // moveUpLeft () {
+    //     this.position[0] -= 2;
+    //     this.position[1] += 1;
+    // }
 
-    moveRightUp () {
-        this.position[0] += 1;
-        this.position[1] += 2;
-    }
+    // moveRightUp () {
+    //     this.position[0] += 1;
+    //     this.position[1] += 2;
+    // }
 
-    moveUpRight () {
-        this.position[0] += 2;
-        this.position[1] += 1;
-    }
+    // moveUpRight () {
+    //     this.position[0] += 2;
+    //     this.position[1] += 1;
+    // }
 
-    moveLeftDown () {
-        this.position[0] -= 1;
-        this.position[1] -= 2;
-    }
+    // moveLeftDown () {
+    //     this.position[0] -= 1;
+    //     this.position[1] -= 2;
+    // }
 
-    moveDownLeft () {
-        this.position[0] -= 2;
-        this.position[1] -= 1;
-    }
+    // moveDownLeft () {
+    //     this.position[0] -= 2;
+    //     this.position[1] -= 1;
+    // }
 
-    moveRightDown () {
-        this.position[0] += 1;
-        this.position[1] -= 2;
-    }
+    // moveRightDown () {
+    //     this.position[0] += 1;
+    //     this.position[1] -= 2;
+    // }
 
-    moveDownRight () {
-        this.position[0] += 2;
-        this.position[1] -= 1;
-    }
+    // moveDownRight () {
+    //     this.position[0] += 2;
+    //     this.position[1] -= 1;
+    // }
 
 }
+
+const knightMoves = (() => {
+    const board = new Board();
+    const knight = new Knight();
+
+    const buildMoveList = knight => {
+        if (board.isValid(knight.position[0], knight.position[1])) return null;
+
+        if (board.isFull()) return null;
+        else {
+            board.mark(knight.position[0], knight.position[1]);
+        }
+    }
+})();
 
 const knight = new Knight(0,0);
 knight.moveDownLeft();
